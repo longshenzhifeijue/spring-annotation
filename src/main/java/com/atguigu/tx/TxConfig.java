70161be8aff6fde2f831ac6005d8ed04548ea2e7
xufei <xufei20160601@163.com>
2021-08-12 22:58:55 +0800

    初始化

--- src/main/java/com/atguigu/tx/TxConfig.java
+++ src/main/java/com/atguigu/tx/TxConfig.java
@@ -73,7 +73,7 @@ public class TxConfig {
 	public DataSource dataSource() throws Exception{
 		ComboPooledDataSource dataSource = new ComboPooledDataSource();
 		dataSource.setUser("root");
-		dataSource.setPassword("123456");
+		dataSource.setPassword("463552783");
 		dataSource.setDriverClass("com.mysql.jdbc.Driver");
 		dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/test");
 		return dataSource;
